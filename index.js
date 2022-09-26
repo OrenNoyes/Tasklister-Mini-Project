5c68b05763c63d80b068fd0110d91b981bda2734
Oren <89209721+OrenNoyes@users.noreply.github.com>
2022-09-26 14:30:39 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+//inputs
+const createdTaskForm = document.getElementById("create-task-form");
+const newTaskDescription = document.getElementById("new-task-description");
+const newTaskPriority = document.getElementById("new-task-priority");
+
+// where the new tasks at
+
+const newTasks = document.getElementById("tasks");
+
+createdTaskForm.addEventListener("submit", spawnTaskForm, );
+});
+
+function spawnTaskForm = event => {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const anotherTask = document.createElement("li")
+  anotherTask.innerHTML = newTaskDescription.value;
+
+
+appendAnotherTask(anotherTask);
+  event.target.reset();
+};
+
+const appendNewestTask = task => {
+  document.getElementById('tasks').appendChild(task);
+};
+
+
